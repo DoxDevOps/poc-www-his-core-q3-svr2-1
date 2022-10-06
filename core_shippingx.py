1bc5c3254ca4894ad05445d7a0cb78767b6ca663
mnaboti <71730801+mnaboti@users.noreply.github.com>
2022-10-06 02:50:54 +0200

    Update core_shippingx.py

--- core_shippingx.py
+++ core_shippingx.py
@@ -37,7 +37,7 @@ def alert(url, params):
 recipients = ["+265998006237", "+265991450316","+265992182669", "+265995246144", "+265998276712", "+265888231289", "+265999959499", "+265995628519", "+265995316633", "+265992268777", "+265993030442"]
 
 #* Get cluster details
-cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/18')
+cluster = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_cluster/19')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52:8000/sites/api/v1/get_single_site/' + str(site_id))
